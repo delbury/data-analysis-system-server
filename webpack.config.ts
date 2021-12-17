968cdaf1ed7141f2267c0c574eae257eb6ee5cc2
hujiqing <hujiqing@bytedance.com>
2021-12-17 16:55:16 +0800

    添加webpack构建

---
+++ webpack.config.ts
@@ -0,0 +1,45 @@
+import { webpack, Configuration, ProgressPlugin } from 'webpack';
+import TerserPlugin from 'terser-webpack-plugin';
+import path from 'path';
+
+const config: Configuration = {
+  mode: 'production',
+  target: 'node',
+  entry: './src/app.ts',
+  output: {
+    path: path.resolve(__dirname, './output'),
+    filename: 'app.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      { test: /\.ts?$/, loader: 'ts-loader' },
+    ],
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+      '~types': path.resolve(__dirname, './types'),
+      '~tables': path.resolve(__dirname, './src/db/tables'),
+      '~libs': path.resolve(__dirname, './libs'),
+    },
+    extensions: ['.ts', '.js', '.json'],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new ProgressPlugin({}),
+  ],
+};
+
+export default config;
